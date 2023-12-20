3d5969b3c8cfda4e50e49e64bcaecca9dbc97ef0
Alex8312 <153901605+Alex8312@users.noreply.github.com>
2023-12-20 21:50:07 +0100

    Update esercizio001.py

--- esercizio001.py
+++ esercizio001.py
@@ -1,1 +1,12 @@
+spesa = {
+  "banane": {"quantita": 7, "prezzo_unitario": 1.98},
+  "farina": {"quantita": 1, "prezzo_unitario": 1.5},
+  "nutella": {"quantita": 2, "prezzo_unitario": 5.69}
+}
 
+for k in spesa.items():
+    print(k)
+   
+print("- Spesa totale quantitativo:",spesa['banane']['quantita'] + spesa['farina']['quantita'] + spesa['nutella']['quantita'], "pezzi")
+
+print("- Prezzo totale della spesa:",spesa['banane']['prezzo_unitario'] + spesa['farina']['prezzo_unitario'] + spesa['nutella']['prezzo_unitario'], "€")
